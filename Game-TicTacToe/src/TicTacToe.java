b9c6f22d75ef5ef0b901a9b57e6ccf21b6c3ff7d
Borhan Uddin <hvr193@usask.ca>
2025-08-11 00:46:20 -0600

    fixed the bug in validity checking Method

--- Game-TicTacToe/src/TicTacToe.java
+++ Game-TicTacToe/src/TicTacToe.java
@@ -1,9 +1,9 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class tickToe {
-    public static void main(String[] args){
-        char [] board ={'0','1','2','3','4','5','6','7','8'};
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[] board = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
 
         System.out.println("Play TICTACTOE");
         printBoard(board);
@@ -13,65 +13,103 @@ public class tickToe {
          * Either X or O
          */
         Random random = new Random();
-        boolean chooseTrun =random.nextBoolean();
-
-
+        boolean chooseTrun = random.nextBoolean();
 
 
         char currentPlayer;
-        if(chooseTrun){
-            currentPlayer='X';
-        }else {
-            currentPlayer='O';
+        if (chooseTrun) {
+            currentPlayer = 'x';
+        } else {
+            currentPlayer = 'o';
         }
 
         Scanner sc = new Scanner(System.in);
-
-
-
 
 
         /**
          * Keeps the game running until there is a winner or draw
          */
 
-        while (winner();){
-            if(currentPlayer=='X'){
+        while (!winner(board)) {
+            if (currentPlayer == 'x') {
                 System.out.println("Its X's turn to play");
                 System.out.print("choose a Number between 0-8: ");
-            }else{
+            } else {
                 System.out.println("Its O's turn to play");
                 System.out.print("choose a Number between 0-8: ");
             }
 
-            int posiBoard= sc.nextInt();
-            board[posiBoard]=currentPlayer;
-            currentPlayer=switchTurn(currentPlayer);
+            int posiBoard = sc.nextInt();
+
+            if(!validateMove(board,posiBoard)){
+                continue;
+            }
+            board[posiBoard] = currentPlayer;
+            currentPlayer = switchTurn(currentPlayer);
+
+
+
+
             printBoard(board);
             System.out.println();
 
         }
 
 
+        System.out.println("\n We have a winner");
 
     }
 
 
-
-
-    static char switchTurn(char currentPlayer){
-        if (currentPlayer=='X'){
-            return 'O';
-        }else {
-            return 'X';
+    static char switchTurn(char currentPlayer) {
+        if (currentPlayer == 'x') {
+            return 'o';
+        } else {
+            return 'x';
         }
 
     }
 
-    static void validateMove(){}
-    static void winner(){
+    static boolean validateMove(char[] board, int position) {
+        if(position<0 || position>8) {
+            System.out.println("Invalid Input,\n try again");
+            return false;
+
+        }
+        if(board[position]=='x' || board[position]=='o'){
+            System.out.println("Spot taken, Choose again");
+            return false;
+
+        }
+        return true;
+    }
+
+
+
+    static boolean winner(char[] board) {
+        return winner('x', board) || winner('o', board);
+    }
+
+
+    static boolean winner(char Player, char[] board) {
+
+        if (board[0] == Player && board[1] == Player && board[2] == Player ||
+                board[3] == Player && board[4] == Player && board[5] == Player ||
+                board[6] == Player && board[7] == Player && board[8] == Player ||
+                board[0] == Player && board[3] == Player && board[6] == Player ||
+                board[1] == Player && board[4] == Player && board[7] == Player ||
+                board[2] == Player && board[5] == Player && board[8] == Player ||
+                board[0] == Player && board[4] == Player && board[8] == Player ||
+                board[2] == Player && board[4] == Player && board[6] == Player) {
+
+            return true;
+        }
+        return false;
 
     }
+
+
+
 
 
 
